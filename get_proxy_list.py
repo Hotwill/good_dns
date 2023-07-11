24c9a9aeffb1312b02a834eca8dd793bf3c9fccd
Hotwill <1946400346@qq.com>
2023-07-11 14:50:17 +0800

    从my_proxy_ruleset拉取自己添加的域名

--- get_proxy_list.py
+++ get_proxy_list.py
@@ -1,3 +1,4 @@
+import re
 import urllib.request
 
 
@@ -16,8 +17,24 @@ def process_file(input_file, output_file):
                 f2.write(line)
 
 
+def process_ruleset(input_file, output_file):
+    with open(input_file, 'r') as f1, open(output_file, 'a') as f2:
+        for line in f1:
+            pattern = r"'\+?\.?(.*?)'"  # 正则表达式模式，匹配位于单引号中间的子串
+            matches = re.findall(pattern, line)
+            if matches:
+                f2.write(matches[0] + '\n')
+
+
 if __name__ == '__main__':
+    dst_filename = 'proxy-list-smartdns.txt'
+
     url = 'https://ghproxy.com/https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/proxy-list.txt'
     filename = '/tmp/proxy-list.txt'
     download_file(url, filename)
-    process_file(filename, 'proxy-list-smartdns.txt')
+    process_file(filename, dst_filename)
+
+    url = 'https://ghproxy.com/https://gist.githubusercontent.com/Hotwill/d04d795b8a72cac7691f24b2fdfaf9b6/raw/my_proxy_ruleset.txt'
+    filename = '/tmp/my_proxy_ruleset.txt'
+    download_file(url, filename)
+    process_ruleset(filename, dst_filename)
